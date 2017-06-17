067b14c19991dace55c17973de7a1033f21ca66f
Pei-Ya Chiu <lockercho@gmail.com>
2017-06-18 00:19:43 +0800

    auto gen plain model

---
+++ src/VideoHandler.cpp
@@ -0,0 +1,9 @@
+//
+//  VideoHandler.cpp
+//  stabilization-for-360
+//
+//  Created by Peiya Chiu on 2017/6/17.
+//  Copyright © 2017年 gpuproject. All rights reserved.
+//
+
+#include "VideoHandler.hpp"
---
+++ src/VideoHandler.h
@@ -0,0 +1,14 @@
+//
+//  VideoHandler.hpp
+//  stabilization-for-360
+//
+//  Created by Peiya Chiu on 2017/6/17.
+//  Copyright © 2017年 gpuproject. All rights reserved.
+//
+
+#ifndef VideoHandler_hpp
+#define VideoHandler_hpp
+
+#include <stdio.h>
+
+#endif /* VideoHandler_hpp */
